222c22d08b90cb9f841868db604ba79b2dc957c1
Prajwal Biradar <112881872+Prajbi@users.noreply.github.com>
2024-09-27 16:54:42 +0530

    Add files via upload

---
+++ addexample.java
@@ -0,0 +1,8 @@
+public class addexample {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 58;
+        int sum = num1 + num2;
+        System.out.println(" the sum is : " + sum);
+    }
+}
